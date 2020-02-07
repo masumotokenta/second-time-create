11b288f1b576ab509d51577ffd965ee5ce438d41
masumotokenta <58218709+masumotokenta@users.noreply.github.com>
2020-02-07 14:34:29 +0900

    ブランチ作成

---
+++ second-time-create/src/application/SampleController.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class SampleController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Butten;
+
+    @FXML
+    void ButtenOnAction1(ActionEvent event) {Scene s = ((Node)event.getSource()).getScene();
+	Window window = s.getWindow();
+	window.hide();
+
+	try {
+		Parent parent = FXMLLoader.load(getClass().getResource("Mypage.fxml"));
+		Scene scene = new Scene(parent);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setTitle("Mypage");
+		stage.show();
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+
+    }
+
+    @FXML
+    void initialize() {
+        assert Butten != null : "fx:id=\"Butten\" was not injected: check your FXML file 'Sample.fxml'.";
+
+    }
+}
